dbc2fbc1192e0eba95c65787f307c4f66071473d
KlausRuediger :^) <akramreiter@student.tgm.ac.at>
2018-12-06 16:49:34 +0100

    Actually forgot to stage some

--- exportPkg/merge.py
+++ exportPkg/merge.py
@@ -1,1 +1,11 @@
 import json #I'll need it sooner or later
+
+def mergeline(objects, keyset):
+    """
+    TODO implement this
+    merges dicts in the objects list into one dataset if the values in the keyset align
+    :param objects: list of input objects
+    :param keyset: keys to determine which attributes have to align for a successful merge
+    :return: merge result
+    """
+    return dict
