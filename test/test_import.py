bd249e8cdbbd5d37958f23aa098592faf7e37314
KlausRuediger :^) <akramreiter@student.tgm.ac.at>
2018-12-06 13:26:29 +0100

    bla

--- test/test_import.py
+++ test/test_import.py
@@ -15,36 +15,52 @@ def resource():
     yield os.path.dirname(os.path.abspath(__file__)) + "/tmp/file.txt"
     shutil.rmtree(os.path.dirname(os.path.abspath(__file__)) + "\\tmp")
 
+@pytest.fixture()
+def testobject():
+    obj = {}
+    obj["firstname"] = {}
+    obj["firstname"]["value"] = "Alexander"
+    obj["firstname"]["validated"] = False
+    obj["lastname"] = {}
+    obj["lastname"]["value"] = "Kramreiter"
+    obj["lastname"]["validated"] = False
+    yield obj
 
 def test_startFile(resource):
     ImportCls.startFile(resource)
     with open(resource) as file:
-        obj = json.loads(file.read(os.path.getsize(resource)) + ']}')
+        obj = json.loads(file.read() + ']}')
         assert obj
         print(str(obj))
+
 
 def test_endFile(resource):
     with open(resource, "w") as file:
         file.write('{"var":[{},\n')
     ImportCls.endFile(resource)
     with open(resource) as file:
-        string = file.read(os.path.getsize(resource))
+        string = file.read()
         print(string)
         obj = json.loads(string)
         assert obj
 
-def test_forward(resource):
-    obj = {}
-    obj["firstname"] = {}
-    obj["firstname"]["value"] = "Alexander"
-    obj["firstname"]["validated"] = False
-    obj["lastname"] = {}
-    obj["lastname"]["value"] = "Alexander"
-    obj["lastname"]["validated"] = False
+
+def test_forward(resource, testobject):
+    obj = testobject
     ImportCls.forward(obj, resource)
     with open(resource) as file:
-        string = file.read(os.path.getsize(resource))
+        string = file.read()
         string = string[:len(string) - 2]
+        print(string)
+        obj = json.loads(string)
+        assert obj
+
+def testcombined(resource, testobject):
+    ImportCls.startFile(resource)
+    ImportCls.forward(testobject, resource)
+    ImportCls.endFile(resource)
+    with open(resource) as file:
+        string = file.read()
         print(string)
         obj = json.loads(string)
         assert obj
