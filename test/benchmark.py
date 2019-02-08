dd29e8c98a856fe4350611f5ca62daf26f02ecaf
mwintersperger-tgm <mwintersperger@student.tgm.ac.at>
2019-02-08 13:55:46 +0100

    ten thousand lines benchmark added

--- test/benchmark.py
+++ test/benchmark.py
@@ -2,24 +2,36 @@ from datetime import datetime
 
 
 from etlController import ETLController
-import argparse
+from shutil import copyfile
 
 
 def main():
     etl = ETLController()
 
+    copyfile("benchmark_ori.json", "benchmark.json")
     start=datetime.now()
     etl.runRules("benchmark.json", 0, 100)
     time = str(datetime.now()-start).lstrip("0:00:")
     print("---------------------")
-    print("Time to validate a hundred entries: %s seconds" % time)
+    print("Time to validate a hundred lines: %s seconds" % time)
     print("---------------------")
 
+    copyfile("benchmark_ori.json", "benchmark.json")
     start=datetime.now()
     etl.runRules("benchmark.json", 0, 1000)
     time = str(datetime.now()-start).lstrip("0:00:")
     print("---------------------")
-    print("Time to validate a thousand entries: %s seconds" % time)
+    print("Time to validate a thousand lines: %s seconds" % time)
+    print("---------------------")
+
+
+    copyfile("benchmark_ori.json", "benchmark.json")
+    start=datetime.now()
+    etl.runRules("benchmark.json", 0, 10000)
+    time = str(datetime.now()-start).lstrip("0:")
+
+    print("---------------------")
+    print("Time to validate ten thousand lines: %s seconds" % time)
     print("---------------------")
 
 if __name__ == '__main__':
