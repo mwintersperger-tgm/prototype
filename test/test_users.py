9ee12fa193fcea922cf91475c17e132d29496af9
mwintersperger-tgm <mwintersperger@student.tgm.ac.at>
2019-02-14 09:31:36 +0100

    user testing

--- test/test_users.py
+++ test/test_users.py
@@ -8,11 +8,43 @@ def controller():
     user = UserController()
     copyfile("user_ori.json", "user.json")
     yield user
-    #os.remove("user.json")
+    os.remove("user.json")
 
 
-def test_textRuleCreation(controller):
+def test_addUser(controller):
     controller.addUser("Michael","123Fiona","User","['AU','US']")
     with open("user.json", "r") as f:
         f.readline()
         assert f.readline() == "{\"name\":\"Michael\", \"pswd\":\"123Fiona\", \"type\":\"User\", \"cc\":\"['AU','US']\"}\n"
+
+def test_addUserMessage(controller):
+    assert controller.addUser("Michael","123Fiona","User","['AU','US']") == "success"
+
+
+def test_addUserNonUniqueName(controller):
+    controller.addUser("Michael","123Fiona","User","['AU','US']")
+    controller.addUser("Michael","123Fiona","User","['AU','US']")
+    with open("user.json", "r") as f:
+        f.readline()
+        f.readline()
+        assert f.readline() == "]}"
+
+def test_addUserNonUniqueNameMessage(controller):
+    controller.addUser("Michael","123Fiona","User","['AU','US']")
+    assert controller.addUser("Michael","123Fiona","User","['AU','US']") == "failure"
+
+def test_listUsernames(controller):
+    controller.addUser("Michael1","123Fiona","User","['AU','US']")
+    controller.addUser("Michael2","123Fiona","User","['AU','US']")
+    assert controller.listUsernames(controller.fileLength()) == ['Michael1', 'Michael2']
+
+def test_removeUser(controller):
+    controller.addUser("Michael","123Fiona","User","['AU','US']")
+    controller.removeUser("Michael")
+    with open("user.json", "r") as f:
+        f.readline()
+        assert f.readline() == "]}"
+
+def test_checkUser(controller):
+    controller.addUser("Michael","123Fiona","User","['AU','US']")
+    assert controller.checkUser("Michael","123Fiona") == "{\"name\":\"Michael\", \"pswd\":\"123Fiona\", \"type\":\"User\", \"cc\":\"['AU','US']\"}"
